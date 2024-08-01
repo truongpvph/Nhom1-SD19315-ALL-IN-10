064a4e13af5571e22bbb0487bb5645769cb234d7
quyenlnph51158 <quyenlnph51158@gmail.com>
2024-08-01 16:36:37 +0700

    aa

--- GUI/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ GUI/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b978732b82365230b446bcd548b4916f501d95fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48bda38dc18ce8c4388e2889db9b0ab102f6548b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GUI/GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
+++ GUI/GUI/obj/Debug/net8.0-windows/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b978732b82365230b446bcd548b4916f501d95fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48bda38dc18ce8c4388e2889db9b0ab102f6548b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
